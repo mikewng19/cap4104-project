9e871a00c91fda668b88ad5fef2ffd64b8effe9a
mikewng19 <mikewng19@gmail.com>
2022-11-04 20:27:21 -0400

    Made some formatting changes. Cleaned up code. Added finishing touches.

--- app.py
+++ app.py
@@ -110,10 +110,6 @@ def process_vaccovid_data(json, array, value):
 
 
 def main():
-    # Date is two day behind to prevent any errors.
-    search_date = datetime.today() - timedelta(days=2)
-    search_date = search_date.strftime('%Y-%m-%d')
-
     # Streamlit
     st.title("CAP 4104 Project")
     st.header("Welcome to the COVID 19 Dashboard!")
@@ -122,19 +118,13 @@ def main():
     with st.expander("More info", expanded=True):
         st.warning("Disclaimer: Data may be inaccurate.", icon="⚠️")
 
-        #radio button
-        option = st.radio( 
-        'Have you been recently affected by COVID 19?',
-        ('Yes', 'No', 'Already had COVID'))
-
-     #Here's what to do if you been affected:
-        if option == 'Yes':
-            st.write("If you or anyone you know has been affected, please folllow CDC guidelines and measures below.")
-
-        #Info box
-            st.info('Click here to see the CDC website: https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/steps-when-sick.html', icon="ℹ️")
-
-        else:
+        # Radio button
+        if st.radio(
+            'Have you been recently affected by COVID 19?',
+                ('Yes', 'No'), index=(1)) == 'Yes':
+            st.write(
+                "If you or anyone you know has been affected, please folllow CDC guidelines and measures below.")
+            # Info box
             st.info('Click here to see the CDC website: https://www.cdc.gov/coronavirus/2019-ncov/if-you-are-sick/steps-when-sick.html', icon="ℹ️")
 
         # Load list of all the states in the US. Even including Puerto Rico (US Territory).
@@ -143,23 +133,30 @@ def main():
         except:
             st.error('Error: Failed to load states.txt', icon="🚨")
 
+    # Organization tabs
     table_tab, map_tab, charts_tab, credits_tab = st.tabs(
         ["Table", "Map", "Charts", "Credits"])
 
     # Table
     with table_tab:
-        st.info(
-            'The API refreshes at certian times: The starting date is set 2 days behind to prevent errors. The starting date may still be invalid.', icon="ℹ️")
         data_table1 = None
+        
+        
+        with st.expander("Show Selection Options", expanded=True):
+            st.info(
+                'The API refreshes at certian times: The starting date is set 2 days behind to prevent errors. The starting date may still be invalid.', icon="ℹ️")
+            date = st.date_input("Select a date: ",
+                                 datetime.today() - timedelta(days=2))
 
-        d = st.date_input("Select a date: ",
-                          datetime.today() - timedelta(days=2))
-        option = st.selectbox("Select a State/Territory: ", states)
-        city_data = get_csse_data(option, "state", d)
+            state_option = st.selectbox("Select a State/Territory: ", states)
+
+        city_data = get_csse_data(state_option, "state", date)
 
         try:
+            st.info('Table data source: COVID-19 Statistics (CSSE) by Axisbits', icon="ℹ️")
             st.header("Showing results for:  " +
                       str(city_data['data'][0]['region']['province']) + "'s Cities")
+
             data_table1 = pd.DataFrame(
                 city_data['data'][0]['region']['cities'])
         except:
@@ -177,10 +174,12 @@ def main():
         latitude, longitude = [], []
         process_csse_map_data(csse_map_data, latitude, 'lat')
         process_csse_map_data(csse_map_data, longitude, 'long')
-
         cords = np.column_stack((latitude, longitude))
-        st.header(
-            "Data Availability Map")
+
+        st.header("Data Availability Map")
+        st.info('Map data source: COVID-19 Statistics (CSSE) by Axisbits', icon="ℹ️") 
+        st.info('This map displays the data availability for the table.', icon="ℹ️")
+
         df_map = pd.DataFrame(cords, columns=['latitude', 'longitude'])
         st.map(df_map)
 
@@ -195,28 +194,31 @@ def main():
         process_vaccovid_data(vaccovid_usa_data, total_deaths, 'total_deaths')
 
         vaccovid_usa_data = pd.DataFrame(vaccovid_usa_data)
+        st.info('Chart data source: VACCOVID - coronavirus, vaccine and treatment tracker by vaccovidlive', icon="ℹ️")
+        st.warning(
+            "Note: Vaccovid API may not be working as intended (Normal Response: Past 6 months of data).", icon="⚠️")
 
         cases_column, death_columns = st.columns((2))
-
         with cases_column:
             st.header("Total Cases [USA]: " + str(max(total_cases)))
         with death_columns:
             st.header("Total Deaths [USA]: " + str(max(total_deaths)))
 
-        st.warning(
-            "Note: Vaccovid API may not be working as intended (Normal Response: Past 6 months of data).", icon="⚠️")
-
         # Line chart
-        if st.checkbox("Show Area Chart", value=True):
+        # Using checkbox to show/hide chart instead of expanders due to issues stated below.
+        if st.checkbox("Show Line Chart", value=True):
             st.header("Line chart [USA]")
 
-            line_chart_options = st.multiselect(
-                'Select data to display on the area chart', ['new_cases', 'new_deaths'], default='new_cases')
+            # Using expander instead of checkbox here due to checkboxes requiring variables to be initialized outside the if statement, 
+            # causing graphs not to show properly.
+            with st.expander("Show Selection Options", expanded=True):
+                line_chart_options = st.multiselect(
+                    'Select data to display on the area chart', ['new_cases', 'new_deaths'], default='new_cases')
 
-            color_cases = st.color_picker(
-                "Select a color for new_cases", '#FFFF00')
-            color_deaths = st.color_picker(
-                "Select a color for new_deaths", '#FF4B4B')
+                color_cases = st.color_picker(
+                    "Select a color for new_cases", '#FFFF00')
+                color_deaths = st.color_picker(
+                    "Select a color for new_deaths", '#FF4B4B')
 
             fig = px.line(
                 vaccovid_usa_data,
@@ -225,54 +227,60 @@ def main():
                 color_discrete_map={
                     'new_cases': color_cases,
                     'new_deaths': color_deaths
-
                 }
             )
             st.plotly_chart(fig, use_container_width=True)
-            if st.button("Open line chart in a new window"):
 
+            # Allows a user to open chart in a new window
+            if st.button("Open line chart in a new window"):
                 fig.show()
 
         # Bar chart
+        # Using checkbox to show/hide chart instead of expanders due to issues stated below.
         if st.checkbox("Show Bar Chart", value=True):
             st.header("Bar chart [USA]")
 
-            bar_chart_options = st.radio('Select data to display on the bar chart',
-                                         ['new_cases', 'new_deaths'])
-            color_bar = st.color_picker(
-                "Select a color for the bar chart", "#FF4B4B")
+            # Using expander instead of checkbox here due to checkboxes requiring variables to be initialized outside the if statement, 
+            # causing graphs not to show properly.
+            with st.expander("Show Selection Options", expanded=True):
+                bar_chart_options = st.radio('Select data to display on the bar chart',
+                                             ['new_cases', 'new_deaths'], horizontal=True)
+                color_bar = st.color_picker(
+                    "Select a color for the bar chart", "#FF4B4B")
 
             fig_bar = px.bar(vaccovid_usa_data, x='date', y=bar_chart_options)
             fig_bar.update_traces(marker_color=color_bar)
 
             st.plotly_chart(fig_bar, use_container_width=True)
 
-            if st.button("Open Bar chart in a new window"):
+            # Allows a user to open chart in a new window
+            if st.button("Open bar chart in a new window"):
                 fig_bar.show()
 
     # Credits
     with credits_tab:
-        st.header("Team:")
-        st.subheader(
-            "Jenniffer Hierro Cruz: [Widget: Radio Button, Feedback/Info messages, Date selector]  \n")
-        st.subheader(
-            "Roberto Luna-Garcia: Line chart, Button, Checkbox [Widgets: Color picker]")
-        st.subheader(
-            "Michael Wong: API requests, API response processing, Button, Table, Map, Bar Chart, Checkbox, Feedback/Info messages, Page layout")
-        st.subheader(
-            "[Widgets: Date selector, Selector box, Multi selector box, Color picker, Radio Button]")
+        with st.expander("Show Team", expanded=True):
+            st.header("Team:")
+            st.subheader(
+                "Jenniffer Hierro Cruz: [Widget: Radio Button, Feedback/Info messages, Date selector]  \n")
+            st.subheader(
+                "Roberto Luna-Garcia: Line chart, Button, Checkbox [Widgets: Color picker]")
+            st.subheader(
+                "Michael Wong: API requests, API response processing, Button, Table, Map, Bar Chart, Checkbox, Feedback/Info messages, Page layout")
+            st.subheader(
+                "[Widgets: Date selector, Selector box, Multi selector box, Color picker, Radio Button]")
+        with st.expander("Show API Credits", expanded=True):
+            st.header("API's Used:")
+            st.subheader(
+                "[Table and Map]:  \nCOVID-19 Statistics (CSSE) by Axisbits:  \nhttps://rapidapi.com/axisbits-axisbits-default/api/covid-19-statistics/")
+            st.subheader("[Charts]:  \nVACCOVID - coronavirus, vaccine and treatment tracker by vaccovidlive:  \nhttps://rapidapi.com/vaccovidlive-vaccovidlive-default/api/vaccovid-coronavirus-vaccine-and-treatment-tracker/")
 
-        st.header("API's Used:")
-        st.subheader(
-            "COVID-19 Statistics (CSSE) by Axisbits:  \nhttps://rapidapi.com/axisbits-axisbits-default/api/covid-19-statistics/")
-        st.subheader("VACCOVID - coronavirus, vaccine and treatment tracker by vaccovidlive:  \nhttps://rapidapi.com/vaccovidlive-vaccovidlive-default/api/vaccovid-coronavirus-vaccine-and-treatment-tracker/")
+            if st.button("Test API Status"):
+                st.write("API Status Codes:  \nInformational responses (100 – 199)  \nSuccessful responses (200 – 299)  \nRedirection messages (300 – 399)  \nClient error responses (400 – 499)  \nServer error responses (500 – 599)")
 
-        if st.button("Test API Status"):
-            st.write("API Status Codes:  \nInformational responses (100 – 199)  \nSuccessful responses (200 – 299)  \nRedirection messages (300 – 399)  \nClient error responses (400 – 499)  \nServer error responses (500 – 599)")
-
-            csse_test = get_csse_data(None, "country", None)
-            vaccovid_test = get_vaccovid_data("USA")
-            del csse_test, vaccovid_test
+                csse_test = get_csse_data(None, "country", None)
+                vaccovid_test = get_vaccovid_data("USA")
+                del csse_test, vaccovid_test
 
 
 if __name__ == '__main__':
